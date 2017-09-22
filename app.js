de0081cb7e42bab22bca5a8bb40c6f97df68a4d4
Miguel Laginha <me@miguellaginha.com>
2017-09-22 11:34:18 +0100

    Cleaning and formatting

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     <bitbar.version>v0.1</bitbar.version>
     <bitbar.author>Miguel Laginha</bitbar.author>
     <bitbar.author.github>brecke</bitbar.author.github>
-    <bitbar.desc>It shows the country flag corresponding to your IP Address, which is useful if you use VPN to route your traffic to other countries.</bitbar.desc>
+    <bitbar.desc>It shows the country flag corresponding to your IP Address (useful when using VPNs).</bitbar.desc>
     <bitbar.image>https://user-images.githubusercontent.com/19879/30689083-d8e4d2bc-9eb7-11e7-8d83-d45a9079c287.png</bitbar.image>
     <bitbar.dependencies>node</bitbar.dependencies>
     <bitbar.abouturl>https://github.com/brecke/bitbar-vpn-flag</bitbar.abouturl>
@@ -22,11 +22,12 @@ https.get('https://ipapi.co/ip/', function(resp){
         body += data;
     });
 
-    resp.on('end', function(){
-	IPAddress = body;
-	ipapi.location((country) => { 
-		console.log(flag(country)); 
-	}, IPAddress.toString(), '', 'country');
+    resp.on('end', function() {
+		IPAddress = body;
+	
+		ipapi.location((country) => {
+			console.log(flag(country)); 
+		}, IPAddress.toString(), '', 'country');
     });
 });
 
